bb2904756ae7e9ae7a02d3383350a9d3f6785bb9
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-18 19:10:58 -0300

    feat(portfolio-frontend): add css variables and suspense in App.jsx

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import './styles/index.css'
+import { Suspense } from 'react'
+
 function App() {
   return (
-    <>
-    </>
+    <Suspense fallback={<div style={{
+      maxWidth: '1280px',
+      margin: '0 auto',
+      padding: '15%',
+      fontSize: 'var(--font-size-lg)',
+      fontWeight: '600'
+    }}> wait until the contend loads </div>} >
+
+      <h1>full stack portfolio</h1>
+
+    </Suspense>
   )
 }
 
