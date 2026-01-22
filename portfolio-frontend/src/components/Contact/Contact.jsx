cbe108338443e47b09f72c1d53d05650df8ec322
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-22 20:44:50 -0300

    refactor(portfolio-frontend): improve footer styles and spacing to center content

--- portfolio-frontend/src/components/Contact/Contact.jsx
+++ portfolio-frontend/src/components/Contact/Contact.jsx
@@ -5,13 +5,13 @@ function Contact() {
         <section className={Styles.information}>
             <p>
                 <i className="fa-solid fa-envelope" aria-hidden="true"></i>
-                <span className="sr-only">Email:</span>
+                <span className="sr-only">My Email</span>
                 <a href="mailto:luiscallejaenrique@gmail.com">luiscallejaenrique@gmail.com</a>
             </p>
             
             <p>
                 <i className="fa-brands fa-github-square" aria-hidden="true"></i>
-                <span className="sr-only">GitHub profile</span>
+                <span className="sr-only">My GitHub profile</span>
                 <a href="https://github.com/LuisDev912" target="_blank" rel="noopener">GitHub</a>
             </p>
         </section>
