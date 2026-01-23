9a96d9cea67e1aa0fed9eada2f8e48bbc29b34f3
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-23 18:41:38 -0300

    feat(portfolio-frontend): improve ProjectCard semantic structure and add an example

--- portfolio-frontend/src/components/ProjectCard/ProjectCard.jsx
+++ portfolio-frontend/src/components/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,47 @@
-// import Styles from './ProjectCard/ProjectCard.module.css';
+import Styles from './ProjectCard.module.css';
+
 
 function ProjectCard({ title, description, image, stack, demoUrl, codeUrl }) {
     return (
-        <article className="card">
-            <div className="imageWrapper">
+        <article className={Styles.card}>
+            <div className={Styles.imageWrapper}>
                 <img src={image} alt={`screenshot of ${title}`} />
+            </div>
+
+            <div className={Styles.content}>
+                <h3>{title}</h3>
+
+                <p>{description}</p>
+                
+                <ul className={Styles.stack}>
+                    {stack.map(tech => (
+                        <li key={tech} className={Styles.stackItem}>
+                            {tech}
+                        </li>
+                    ))}
+                </ul>
+
+                <div className={Styles.links}>
+                    {demoUrl && (
+                        <a
+                            href={demoUrl}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Live demo
+                        </a>
+                    )}
+
+                    {codeUrl && (
+                        <a
+                            href={codeUrl}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Code
+                        </a>
+                    )}
+                </div>
             </div>
         </article>
     )
--- portfolio-frontend/src/pages/Home.jsx
+++ portfolio-frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import Hero from "../components/Hero"
 import Learning from "../components/Learning/Learning.jsx"
+import ProjectCard from "../components/ProjectCard/ProjectCard.jsx"
 
 export default function HomePage() {
     return (
         <>
             <Hero />
             <Learning />
+            <ProjectCard
+                title='example'
+                description='Description example for the card'
+                image='blank'
+                stack={['React', 'i18n']}
+                demoUrl='http://page.host.com'
+                codeUrl='https://github.com/example'
+            
+            />
         </>
     )
 }
