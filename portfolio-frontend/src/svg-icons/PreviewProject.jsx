0baf7b165a8555119a893db8e2a9ee1076f2fabd
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-24 09:31:41 -0300

    feat(portfolio-frontend): import a new svg icon for the GitHub link an rename PreviewExample

---
+++ portfolio-frontend/src/svg-icons/PreviewProject.jsx
@@ -0,0 +1,5 @@
+export function PreviewProject() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 -8 32 32"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
+    )
+}
