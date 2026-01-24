8f5435f2de972e72a947f17779a5b1d1fe3d3cdf
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-24 10:38:06 -0300

    feat(portfolio-frontend): display a new project with ProjectCard and a preview of the project

--- portfolio-frontend/src/pages/Home.jsx
+++ portfolio-frontend/src/pages/Home.jsx
@@ -11,13 +11,20 @@ export default function HomePage() {
                 <h3 className="heading-secondary">Projects</h3>
                 <ProjectCard
                     title='Fast Shopping List - Create your grocery list and calculate prices easily.'
-                    description='Fast Shopping List is a simple frontend project that lets you create shopping lists and calculate total prices easily. You can insert the name of a product, its price, and the amount of units or packages.
+                    description='A simple frontend page that lets you create shopping lists and calculate total prices easily. You can insert the name of a product, its price, and the amount of units or packages.
                     The app automatically calculates the total cost and saves your lists in the browser using localStorage.'
                     image='/images/shop-list-preview.webp'
-                    stack={['React', 'i18n']}
+                    stack={['JavaScript', 'HTML', 'CSS']}
                     demoUrl='https://luisdev912.github.io/shop-list/'
                     codeUrl='https://github.com/LuisDev912/shop-list'
-                
+                />
+
+                <ProjectCard
+                    title='Rosary guide - learn how to pray the Holy Rosary step by step'
+                    description='A modern, accessible and multilingual guide for praying the Holy Rosary step by step with a simple, clear and responsive style. Contains an information page about the basics of the Rosary with a page that helps while praying'
+                    image='/images/Rosary-guide-preview.webp'
+                    stack={['React', 'i18n', 'CSS Modules', 'Accessibility']}
+                    codeUrl='https://github.com/LuisDev912/Rosary-guide'
                 />
             </section>
         </>
