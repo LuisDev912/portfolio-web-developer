4f0458a5776b71e16b60bf27813b69dcebe9f77c
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-25 17:43:56 -0300

    feat(portfolio-frontend): display the projects in Projects page

--- portfolio-frontend/src/pages/Projects.jsx
+++ portfolio-frontend/src/pages/Projects.jsx
@@ -1,5 +1,11 @@
+import { projects } from "../data/projects";
+import ProjectsSection from "../components/ProjectsSection/ProjectsSection";
+
+
 export default function Projects() {
     return (
-        <></>
-    )
-}
+        <>
+            <ProjectsSection title='Projects' projects={projects} />
+        </>
+    );
+};
