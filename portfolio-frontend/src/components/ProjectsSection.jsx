86e7ddfd54bc38564412e5a406de8864b213e8bd
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-25 16:58:13 -0300

    feat(portfolio-frontend): create new files for /projects page - Create a projects.js file that contains the information for each project card. - Create a ProjectsSection.jsx component to display a container of filtered cards.

---
+++ portfolio-frontend/src/components/ProjectsSection.jsx
@@ -0,0 +1,10 @@
+function ProjectsSection({ title, projects }) {
+    return (
+        <section>
+            <h3 className="heading-secondary">{title}</h3>
+
+            {/* here the projects will be displayed */}
+        </section>
+    )
+}
+export default ProjectsSection;
---
+++ portfolio-frontend/src/data/projects.js
@@ -0,0 +1,6 @@
+// this file holds all the data that each card of ProjectCard.jsx
+// have to display, containing the id, title, stack, etc.
+
+export const projects = [
+    { id: 1, featured: true}
+]
