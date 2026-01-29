312ada45a3ef6e8db1241f70582b507d62a29089
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-29 11:06:01 -0300

    feat(portfolio-frontend): create two new components for the nav

---
+++ portfolio-frontend/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import NavItem from "./NavItem";
+
+function Nav() {
+    return (
+        <nav>
+            {/* this component will contain different nav items that lead to different parts of the app */}
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ portfolio-frontend/src/components/NavItem.jsx
@@ -0,0 +1,5 @@
+function NavItem() {
+    // this component will show either a section link or a different page URL
+}
+
+export default NavItem;
