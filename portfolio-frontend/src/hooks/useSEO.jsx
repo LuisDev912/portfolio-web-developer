3ed19388c32e9b5b35a1021520ee91ac762ebefc
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-31 11:15:24 -0300

    feat(portfolio-frontend): improve SEO by using OG tags and useSEO logic

---
+++ portfolio-frontend/src/hooks/useSEO.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+
+function setMeta({ name, property, content }) {
+    let meta;
+
+    if (name) {
+        meta = document.querySelector(`meta[name="${name}"]`);
+    } else if (property) {
+        meta = document.querySelector(`meta[property="${property}"]`);
+    }
+
+    if (!meta) {
+        meta = document.createElement("meta");
+        if (name) meta.setAttribute("name", name);
+        if (property) meta.setAttribute("property", property);
+        document.head.appendChild(meta);
+    }
+
+    meta.setAttribute("content", content);
+}
+
+export function useSEO({ title, description, og, noIndex = false }) {
+
+    useEffect(() => {
+        if (title) {
+            document.title = title;
+        }
+
+        if (description) {
+            setMeta({
+                name: "description",
+                content: description,
+            });
+        }
+
+        setMeta({
+            name: "robots",
+            content: noIndex ? "noindex, follow" : "index, follow",
+        });
+
+        if (og?.title) {
+            setMeta({
+                property: "og:title",
+                content: og.title,
+            });
+        }
+
+        if (og?.description) {
+            setMeta({
+                property: "og:description",
+                content: og.description,
+            });
+        }
+    }, [title, description, og, noIndex]);
+}
