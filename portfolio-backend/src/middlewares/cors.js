6aeabba3421e13bad4498ed7ef1f269efeb98ae2
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-01 15:30:54 -0300

    feat(portfolio-backend): create new folders and create a cors middleware

---
+++ portfolio-backend/src/middlewares/cors.js
@@ -0,0 +1,18 @@
+import cors from 'cors'
+
+const ACCEPTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5173'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
+    return cors({
+        origin: (origin, callback) => {
+            if (acceptedOrigins.includes(origin) || !origin) {
+                return callback(null, true)
+            }
+
+            return callback(new Error('Forbidden origin.'))
+        }
+    });
+};
