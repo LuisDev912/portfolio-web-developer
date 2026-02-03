58db312089e1d1553b6ced5b8f302616d95e47f0
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-03 12:35:18 -0300

    feat: update package.json and avoid hardcoded URL

--- portfolio-frontend/src/services/api.js
+++ portfolio-frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001/api'; // Having hard coded APIs is not a good practice. This will be changed
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getProjects() {
     const res = await fetch(`${API_URL}/projects`);
